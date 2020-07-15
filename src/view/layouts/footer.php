6a3a86179e78e5376d966921e4b3f398c6b2a73e
Alan <nalax2@gmail.com>
2020-07-15 16:23:25 +0200

    Update link fontawesome

---
+++ src/view/layouts/footer.php
@@ -0,0 +1,17 @@
+
+    </div>
+
+    <footer>
+                                                <!-- Tab Bar Mobile -->
+        <div class="tab-bottom-bar">
+            <a href="#" class="tab-menu-item tab-current"><i class="fas fa-home"></i></a>
+            <a class="tab-nav-indicator"></a>
+            <a href="#" class="tab-menu-item"><i class="fas fa-book-reader"></i></a>
+            <a href="#" class="tab-menu-item"><i class="fas fa-user-tie"></i></a>
+            <a href="#" class="tab-menu-item"><i class="fas fa-at"></i></a>
+        </div>
+    </footer>
+
+    <script src="js/TabBar.js"></script>
+    </body>
+</html>
---
+++ src/view/layouts/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <!-- Open Graph / Facebook -->
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="" />
+        <meta property="og:title" content="" />
+        <meta property="og:description"
+              content="" />
+        <meta property="og:image" content="" />
+        <!-- Twitter -->
+        <meta property="twitter:card" content="" />
+        <meta property="twitter:url" content="" />
+        <meta property="twitter:title" content="" />
+        <meta property="twitter:description"
+              content="" />
+        <meta property="twitter:image" content="" />
+        <title>Jean Forteroche</title>
+        <link rel="shortcut icon" href="">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.1/css/all.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+    </head>
+    <body>
+
+    <header>
+        <nav>
+            <ul>
+                <li>Accueil</li>
+                <li>Article</li>
+                <li>A propos de moi</li>
+                <li>Contact</li>
+                <li>Se connecter</li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="container">
