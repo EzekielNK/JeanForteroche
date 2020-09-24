252d8b7c2b11119fd7edb8a16c3124ef9354e8d8
Alan <nalax2@gmail.com>
2020-09-24 17:10:22 +0200

    refactoring

--- src/view/post/index.php
+++ src/view/post/index.php
@@ -1,14 +1,14 @@
 <?php
 
+require dirname(dirname(dirname(__DIR__))) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'DataPDO.php';
 use App\Model\PostTable;
+
 $title = 'Jean Forteroche : Accueil';
-require dirname(dirname(dirname(__DIR__))) . DIRECTORY_SEPARATOR . 'config' .DIRECTORY_SEPARATOR . 'DataPDO.php';
 
 $post = new PostTable();
 $posts = $post->getPost();
-while ($article = $posts->fetch()){
+while ($article = $posts->fetch()) {
     echo $article['title'];
-    dd($article);
 };
 
 ?>
