63e0b4858105d2c8ba9b2245cda60551811ec7cf
Alan <nalax2@gmail.com>
2020-09-30 20:02:14 +0200

    Add card post and css

---
+++ src/view/post/card.php
@@ -0,0 +1,8 @@
+<article class="card">
+    <div class="card-body">
+        <figure class="card-image"><img src="//placehold.it/200" class="img-fluid" alt=""></figure>
+        <h4 class="card-title"><?= $post->title ?></h4>
+        <p class="text-muted"><?= $post->created_at ?></p>
+        <p><?= nl2br($post->content) ?></p>
+    </div>
+</article>
