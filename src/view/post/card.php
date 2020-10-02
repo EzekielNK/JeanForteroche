56fc1026979f3a20fa0e295895f361ebb0f7e172
Alan <nalax2@gmail.com>
2020-10-02 15:27:30 +0200

    Grid model test

--- src/view/post/card.php
+++ src/view/post/card.php
@@ -1,7 +1,10 @@
 <article class="card">
     <div class="card-body">
         <figure class="card-image"><img src="//placehold.it/200" class="img-fluid" alt=""></figure>
-        <h4 class="card-title"><?= $post->title ?></h4>
+        <h4 class="card-title">
+            <?= $post->title ?>
+            <hr>
+        </h4>
         <p class="text-muted"><?= $post->created_at ?></p>
         <p><?= nl2br($post->content) ?></p>
     </div>
