53964f73bd8e6b59a620eb3b1737b393b01f2c22
Alan <nalax2@gmail.com>
2020-10-06 15:00:06 +0200

    grid test

--- src/view/post/card.php
+++ src/view/post/card.php
@@ -1,11 +1,19 @@
+<?php
+
+
+use App\Helpers\Text;
+
+
+?>
 <article class="card">
     <div class="card-body">
-        <figure class="card-image"><img src="//placehold.it/200" class="img-fluid" alt=""></figure>
+        <figure class="card-image"><img src="" class="img-fluid" alt=""></figure>
         <h4 class="card-title">
             <?= $post->title ?>
             <hr>
         </h4>
         <p class="text-muted"><?= $post->created_at ?></p>
-        <p><?= nl2br($post->content) ?></p>
+        <p class="card-info"><?= nl2br(Text::excerpt($post->content, 150)) ?></p>
+        <button type="button" class="btn-button">Voir plus</button>
     </div>
 </article>
